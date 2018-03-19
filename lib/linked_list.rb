041cb095b91be500b93308721452cd63e0200944
Manoj Panta <manojpanta95@gmail.com>
2018-03-19 11:14:51 -0600

    append method created along with count

--- lib/linked_list.rb
+++ lib/linked_list.rb
@@ -1,7 +1,19 @@
 require './lib/node'
 
 class LinkedList
-  
+
+  attr_reader :head, :count
+
+  def initialize
+    @head = nil
+    @count = 0
+  end
+
+  def append(data)
+    @count += 1
+    @head = Node.new(data)
+  end
+
 
 
 
--- test/linked_list_test.rb
+++ test/linked_list_test.rb
@@ -4,10 +4,30 @@ require 'minitest/pride'
 require 'pry'
 
 class LinkedListTest < MiniTest::Test
+
   def test_if_it_exists
     list = LinkedList.new
     assert_instance_of LinkedList, list
   end
+
+  def test_if_head_method_works
+    list = LinkedList.new
+    assert_nil list.head
+  end
+
+  def test_if_append_method_works
+    list = LinkedList.new
+    list.append("doop")
+    assert_equal "doop", list.head.data
+  end
+
+  def test_count_method_is_working
+    list = LinkedList.new
+    list.append("doop")
+    assert_equal 1, list.count
+  end
+
+
 
 
 
